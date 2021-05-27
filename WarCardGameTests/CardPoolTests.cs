2b332288fbd72496af2ffa76bcf07b13f75f78f7
Mark Taylor <marktaylor@outlook.com>
2021-05-27 16:33:33 -0700

    Couple of basic tests for the card pool

---
+++ WarCardGameTests/CardPoolTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace WarCardGame.Tests
+{
+    public class CardPoolTests
+    {
+        [Fact]
+        public void VerifyPoolCreation()
+        {
+            var cardPool = new CardPool();
+            Assert.Null(cardPool.CurrentFaceUpCard(0));
+        }
+
+        [Fact]
+        public void ClaimCardsOnEmptyPool()
+        {
+            var cardPool = new CardPool();
+            PlayingCard[] cards = cardPool.ClaimCards();
+
+            Assert.True(cards.Length == 2);
+            Assert.Null(cards[0]);
+            Assert.Null(cards[1]);
+        }
+    }
+}
